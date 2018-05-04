70db7bee028f5a47e95d89ee6e9c78161a215352
nokkvikarlsson <nokkvi17@ru.is>
2018-05-04 14:09:32 +0000

    partial server side validation for log in added

--- Models/InputModels/LoginInputModel.cs
+++ Models/InputModels/LoginInputModel.cs
@@ -5,10 +5,11 @@ namespace TheBookCave.Models.InputModels
     public class LoginInputModel
     {
         [EmailAddress]
+        [Required]
         public string Email { get; set; }
-
+        [Required]
         public string Password { get; set; }
-
+        [Required]
         public bool RememberMe { get; set; }
     }
 }
