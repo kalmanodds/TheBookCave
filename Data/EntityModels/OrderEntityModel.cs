3051a26fe6a181efb6b955251db0455cb911ea3d
Atli Gislason <atlig17@ru.is>
2018-05-06 18:12:53 +0000

    fixed database. userIds are string

--- Data/EntityModels/OrderEntityModel.cs
+++ Data/EntityModels/OrderEntityModel.cs
@@ -5,7 +5,7 @@ namespace TheBookCave.Data.EntityModels.OrderEntityModel
     public class OrderEntityModel
     {
         public int ID { get; set; }
-        public int UserID { get; set; }
+        public string UserID { get; set; }
         public double TotalPrice { get; set; }
         public AddressModel ShippingAddress { get; set; }
         public DateModel DateOrder { get; set; }
--- Data/EntityModels/RatingEntityModel.cs
+++ Data/EntityModels/RatingEntityModel.cs
@@ -5,7 +5,7 @@ namespace TheBookCave.Data.EntityModels.RatingEntityModel
         public int ID { get; set; }
         public int Score { get; set; }
         public string Comment { get; set; }
-        public int UserID { get; set; }
+        public string UserID { get; set; }
         public int BookID { get; set; }
     }
 }
