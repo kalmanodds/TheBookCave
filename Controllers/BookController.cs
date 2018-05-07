6562daf9ab2c0a81031498cf0d7999827aeb9032
Atli Gislason <atlig17@ru.is>
2018-05-07 23:56:00 +0000

    commented BookController

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -7,62 +7,74 @@ namespace TheBookCave.Controllers
 {
     public class BookController : Controller
     {
+        //Private member variables of what databases this controller can access.
         private BookService _bookService;
         private RatingService _ratingService;
 
+        //Constructor that initializes Services.
         public BookController(){
             _bookService = new BookService();
             _ratingService = new RatingService();
         }
 
+        // /Book redirects to the Catalogue.
         public IActionResult Index()
         {
             return RedirectToAction("Catalogue", "Home");
         }
 
-        //NokkviKilla made this so this does not work
+        //NokkviKilla made this so this does work.
+        //The function receives the details of one book.
         public IActionResult Details(int? id)
         {
             //This function takes in a bookID parameter which is the ID of the book that should have details.
             if(id == null)
             {
-                //If a book is not specified, redirect to the not found page
+                //If a book is not specified, redirect to the not found page.
                 return View("NotFound");
             }
-            
+
+            //Returns the BookViewModel of the book.
             var book = _bookService.GetBook((int)id);
 
             if(book == null){
                 return View("NotFound");
             }
 
-            //should return View with book as parameter
+            //Returns the Details View for the book.
             return View(book);
         }
-
-        public IActionResult Rating(int bookID)
+        
+        //TODO
+        //The Rating Page for the Book.
+        public IActionResult Rating(int id)
         {
             //This function takes in a bookID parameter which is the ID of the book that should have ratings.
-            if(bookID == 0)
+            if(id == 0)
             {
-                //If a book is not specified, redirect to the catalogue
+                //If a book is not specified, redirect to the catalogue.
                 RedirectToAction("Home/Catalogue");
             }
-            //var book = getBook(bookID);
-            //should return View with book as parameter
+            
+            var book = _bookService.GetBook(id);
+            
             return View();
         }
 
+        //TODO
+        //Editor for Books.
+        //Should only be accessable by Employees.
         public IActionResult Edit(int bookID)
         {
             //This function takes in a bookID parameter which is the ID of the book that should be edited.
             if(bookID == 0)
             {
-                //If a book is not specified, redirect to the catalogue
+                //If a book is not specified, redirect to the catalogue.
                 RedirectToAction("Home/Catalogue");
             }
-            //var book = getBook(bookID);
-            //should return View with book as parameter
+            
+            var book = _bookService.GetBook(bookID);
+            
             return View();
         }
     }
