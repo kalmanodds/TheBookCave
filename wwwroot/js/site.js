93a5b42ec10d639d741b1920c59899735d3c6705
nokkvikarlsson <nokkvi17@ru.is>
2018-05-10 18:08:50 +0000

    Remove from cart and remove from wishlist both have a cool animation now when you press the button

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,9 @@
 $(".add-to-cart").click(function(e) {
     sweetAlert("Book has been added to your cart!", "", "success")
 });
+$(".remove-from-cart").click(function(e) {
+    sweetAlert("Book has been removed from your cart!", "", "success")
+});
+$(".remove-from-wishlist").click(function(e) {
+    sweetAlert("Book has been removed from your wishlist!", "", "success")
+});
